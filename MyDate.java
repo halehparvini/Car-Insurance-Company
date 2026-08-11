43b9b2b2ba9e025667623ce322abd82e397324bd
Haleh Parvini <parvinihaleh@gmail.com>
2026-08-11 11:24:06 +0330

    Adding setters and getters.

--- MyDate.java
+++ MyDate.java
@@ -12,4 +12,34 @@ public class MyDate
         this.month = month;
         this.day = day;
     }
+
+    public int getYear ()
+    {
+        return year;
+    }
+
+    public int getMonth ()
+    {
+        return month;
+    }
+
+    public int getDay ()
+    {
+        return day;
+    }
+
+    public void setYear (int year)
+    {
+        this.year = year;
+    }
+
+    public void setMonth (int month)
+    {
+        this.month = month;
+    }
+
+    public void setDay (int day)
+    {
+        this.day = day;
+    }
 }
