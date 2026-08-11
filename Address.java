9aaf7d4bc496fcd92e13f1c5051922bde9705cd8
Haleh Parvini <parvinihaleh@gmail.com>
2026-08-11 11:07:12 +0330

    Adding a new class named Address with attributes and a constructor.

---
+++ Address.java
@@ -0,0 +1,18 @@
+package WEEK1;
+
+public class Address
+{
+    private int streetNum;
+    private String street;
+    private String suburb;
+    private String city;
+
+    public Address (int streetNum, String street, String suburb, String city)
+    {
+        this.streetNum = streetNum;
+        this.street = street;
+        this.suburb = suburb;
+        this.city = city;
+    }
+    
+}
