3d73842013e4cda78e71add714452065c1b00851
Haleh Parvini <parvinihaleh@gmail.com>
2026-08-12 14:03:43 +0330

    Adding a toString method to the class.

--- Address.java
+++ Address.java
@@ -54,4 +54,10 @@ public class Address
     {
         this.city = city;
     }
+
+    @Override
+    public String toString ()
+    {
+        return streetNum + " " + street + ", " + suburb + ", " + city;
+    }
 }
