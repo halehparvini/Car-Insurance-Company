43379f8fe83e316944a902aad390931789620ff3
Haleh Parvini <parvinihaleh@gmail.com>
2026-08-18 23:18:28 +0330

    Adding fields, constructors, mutators and assessors to the class.

--- InsuranceCompany.java
+++ InsuranceCompany.java
@@ -1,5 +1,46 @@
 package WEEK1;
 
-public class InsuranceCompany {
-    
+import java.util.ArrayList;
+
+public class InsuranceCompany
+{
+    private String name;
+    private ArrayList <User> users;
+    private String adminUsername; 
+    private String adminPassword; 
+    private int flatRate;
+
+    public InsuranceCompany (String name, String adminUsername, String adminPassword, int flatRate)
+    {
+        this.name = name;
+        users = new ArrayList<>();
+        this.adminUsername = adminUsername;
+        this.adminPassword = adminPassword;
+        this.flatRate = flatRate; 
+    }
+
+    public String getName ()
+    {
+        return name;
+    }
+
+    public String getAdminUsername ()
+    {
+        return adminUsername;
+    }
+
+    public String getAdminPassword ()
+    {
+        return adminPassword;
+    }
+
+    public int getFlatRate ()
+    {
+        return flatRate;
+    }
+
+    public void setAdminUsername (String adminUsername)
+    {
+        this.adminUsername = adminUsername;
+    }
 }
